e21120376fd26d761e3dc5fec5d94af6a9b31489
Mr-G-D <daredinesh00@gmail.com>
2023-01-20 14:25:31 +0530

    :package: all packages installed

---
+++ client/src/config/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByzCGAGsXRE7eMpyd_A18gLKJ7-Dk7wkk",
+  authDomain: "crypto-portfolio-6dced.firebaseapp.com",
+  projectId: "crypto-portfolio-6dced",
+  storageBucket: "crypto-portfolio-6dced.appspot.com",
+  messagingSenderId: "279818555115",
+  appId: "1:279818555115:web:65c63a872f25bb78aef8ae",
+};
+
+export const app = initializeApp(firebaseConfig);
