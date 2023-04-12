3979e6732b785067ee1187c24053d8e44e19229e
Mr-G-D <daredinesh00@gmail.com>
2023-04-12 14:19:50 +0530

    firebase

---
+++ client/src/config/db.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: "AIzaSyByzCGAGsXRE7eMpyd_A18gLKJ7-Dk7wkk",
+  authDomain: "crypto-portfolio-6dced.firebaseapp.com",
+  projectId: "crypto-portfolio-6dced",
+  storageBucket: "crypto-portfolio-6dced.appspot.com",
+  messagingSenderId: "279818555115",
+  appId: "1:279818555115:web:65c63a872f25bb78aef8ae",
+};
+export default config;
