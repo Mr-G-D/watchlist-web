36df8c7fc71ac395a98d1423a3fd45c5b71ae42a
Mr-G-D <daredinesh00@gmail.com>
2023-04-22 16:38:39 +0530

    :alien:

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ app.use(
     origin: "*",
   }),
 );
+const allowCrossDomain = (req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `example.com`);
+  res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+  res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+  next();
+};
+
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
