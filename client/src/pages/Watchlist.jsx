2e1b5366caccf38b6400788c7fe1ea53218b234e
Mr-G-D <daredinesh00@gmail.com>
2023-04-23 22:11:20 +0530

    :wrench:

--- client/src/pages/Watchlist.jsx
+++ client/src/pages/Watchlist.jsx
@@ -6,6 +6,7 @@ import { backendGet } from "../main/axios";
 import secureLocalStorage from "react-secure-storage";
 import { TfiClose } from "react-icons/tfi";
 import { useNavigate } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Watchlist = () => {
   const [movies, setMovies] = useState([]);
@@ -43,7 +44,12 @@ const Watchlist = () => {
         setSidebar(false);
       }
     };
+    const checkType = async () => {
+      const type = localStorage.getItem("type") === "movie" ? 1 : 0;
+      if (type == movies[0].movie) console.log(type);
+    };
     document.addEventListener("mousedown", handle);
+    checkType();
   });
 
   return (
@@ -58,7 +64,7 @@ const Watchlist = () => {
       <div
         ref={sidebarRef}
         className={`basis-1/5 border-slate-200 bg-white z-10 fixed translate-x-0 ease-in-out duration-300 ${
-          sidebar ? "absolute" : "hidden"
+          sidebar ? "fixed" : "hidden"
         } xl:block xl:static `}
         style={{
           borderRightStyle: "solid",
@@ -69,6 +75,15 @@ const Watchlist = () => {
         <Sidebar genre={genres} setSidebar={setSidebar} />
       </div>
       <div className="w-full xl:w-[80%] xl:basis-4/5 mt-16">
+        <div className="m-5 sm:m-10 absolute top-1.5 left-0">
+          <div className="xl:hidden">
+            <GiHamburgerMenu
+              onClick={() => setSidebar(true)}
+              size={20}
+              className="mx-4"
+            />
+          </div>
+        </div>
         <div>
           <div
             className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 overflow-auto"
